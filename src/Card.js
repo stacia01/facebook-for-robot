e33ce624de94b711fa30fc74ebf015f28bb902b0
unknown <staciarimta@gmail.com>
2021-02-10 07:38:58 +0700

    new search bar added

--- src/Card.js
+++ src/Card.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Card = ({name, email, id}) => {
   	return (
-	  	<div class="bg-light-green dib br3 mw5 pa3 ma2 grow bw2 shadow">
+	  	<div className="bg-light-green dib br3 mw5 pa3 ma2 grow bw2 shadow-3">
 			<img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
-			<h1 class="f5 mb2">{name}</h1>
-		    <h2 class="f5 fw4 grey mt0">{email}</h2>			 
+			<h1 className="f5 mb2">{name}</h1>
+		    <h2 className="f5 fw4 grey mt0">{email}</h2>			 
 		</div>
   	);
 }
--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import {robots} from './robots';
-import CardList from './CardList'
+import CardList from './CardList';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-  	<CardList robots={robots}/>
+  	<App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
